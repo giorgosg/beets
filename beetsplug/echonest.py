15683ec55b876548db83a7757ad8acc90dbf31bc
Peter Schnebel <pschnebel@gmail.com>
2013-10-18 12:43:07 +0200

    Fine tuning + added proof of concept for mapping on the fly.

--- beetsplug/echonest.py
+++ beetsplug/echonest.py
@@ -1,31 +1,82 @@
+import time
 import logging
+import socket
 
-from beets import util, config, plugins
+from beets import util, config, plugins, ui
 import pyechonest
 import pyechonest.song
 import pyechonest.track
 
 log = logging.getLogger('beets')
 
+RETRY_INTERVAL = 10  # seconds
+RETRIES = 10
+
+def mapper(field, mapping, min_v=0.0, max_v=1.0):
+    def fieldfunc(item):
+        try:
+            value = item.get(field, None)
+            if value is None:
+                return u'not set'
+            value = float(value)
+            inc = (max_v - min_v) / len(mapping)
+            i = min_v
+            for m in mapping:
+                i += inc
+                if value < i:
+                    return m
+            return m # in case of floating point precision problems
+        except ValueError:
+            return item.get(field)
+    return fieldfunc
 
 class EchonestMetadataPlugin(plugins.BeetsPlugin):
     _songs = {}
 
     def __init__(self):
         super(EchonestMetadataPlugin, self).__init__()
-        self.config.add({'auto': True, 'apikey': '', 'codegen': ''})
+        self.config.add({'auto': True, 'apikey': u'', 'codegen': u''})
         pyechonest.config.ECHO_NEST_API_KEY = \
             config['echonest']['apikey'].get(unicode)
-        pyechonest.config.CODEGEN_BINARY_OVERRIDE = \
-            config['echonest']['codegen'].get(unicode)
+        if config['echonest']['codegen'].get(unicode) != u'':
+            pyechonest.config.CODEGEN_BINARY_OVERRIDE = \
+                config['echonest']['codegen'].get(unicode)
         self.register_listener('import_task_start', self.fetch_song_task)
         self.register_listener('import_task_apply', self.apply_metadata_task)
 
-    def fingerprint(self, item):
-        if not getattr(item, 'echonest_fingerprint', False):
+        self.template_fields['speechiness'] = mapper('speechiness',
+                ['singing', 'rapping', 'speaking'])
+        self.template_fields['danceability'] = mapper('danceability',
+                ['bed', 'couch', 'party', 'disco'])
+
+    def _echofun(self, func, **kwargs):
+        for i in range(RETRIES):
             try:
-                code = pyechonest.util.codegen(item.path)
-                item.echonest_fingerprint = code[0]['code']
+                result = func(**kwargs)
+            except pyechonest.util.EchoNestAPIError as e:
+                if e.code == 3:
+                    # reached access limit per minute
+                    time.sleep(RETRY_INTERVAL)
+                else:
+                    log.error(u'echonest: {0}'.format(e.args[0][0]))
+                    return None
+            except (pyechonest.util.EchoNestIOError, socket.error) as e:
+                log.warn(u'echonest: IO error: {0}'.format(e))
+                time.sleep(RETRY_INTERVAL)
+            else:
+                break
+        else:
+            # If we exited the loop without breaking, then we used up all
+            # our allotted retries.
+            raise Exception(u'exceeded retries')
+            return None
+        return result
+
+    def fingerprint(self, item):
+        if item.get('echonest_fingerprint', None) is None:
+            try:
+                code = self._echofun(pyechonest.util.codegen, filename=item.path)
+                item['echonest_fingerprint'] = code[0]['code']
                 item.write()
             except Exception as exc:
                 log.error('echonest: fingerprinting failed: {0}: {1}'
@@ -35,17 +86,18 @@ class EchonestMetadataPlugin(plugins.BeetsPlugin):
 
     def analyze(self, item):
         try:
-            track = pyechonest.track.track_from_filename(item.path)
-            pyechonest.song.profile(track_ids=[track.id])
+            track = self._echofun(pyechonest.track.track_from_filename,
+                    filename=item.path)
+            self._echofun(pyechonest.song.profile, track_ids=[track.id])
         except Exception as exc:
             log.error('echonest: analysis failed: {0}: {1}'
                       .format(util.syspath(item.path), str(exc)))
 
     def identify(self, item):
         try:
-            songs = pyechonest.song.identify(code=self.fingerprint(item))
+            songs = self._echofun(pyechonest.song.identify, code=self.fingerprint(item))
             if not songs:
-                raise Exception('no songs found')
+                raise Exception(u'no songs found')
             return max(songs, key=lambda s: s.score)
         except Exception as exc:
             log.error('echonest: identification failed: {0}: {1}'
@@ -53,12 +105,22 @@ class EchonestMetadataPlugin(plugins.BeetsPlugin):
 
     def search(self, item):
         try:
-            songs = pyechonest.song.search(title=item.title,
-                                           artist=item.artist,
-                                           buckets=['id:musicbrainz', 'tracks'])
-            if not songs:
-                raise Exception('no songs found')
-            return songs[0]
+            songs = self._echofun(pyechonest.song.search, title=item.title,
+                    artist=item.artist, buckets=['id:musicbrainz', 'tracks'])
+            pick = None
+            if songs:
+                min_dist = item.length
+                for song in songs:
+                    if song.artist.name.lower() == item.artist.lower() \
+                            and song.title.lower() == item.title.lower():
+                        dist = abs(item.length - song.audio_summary['duration'])
+                        if dist < min_dist:
+                            min_dist = dist
+                            pick = song
+            if pick is None:
+                raise Exception(u'no songs found')
+            log.info(u'echonest: candidate distance {0}'.format(min_dist))
+            return pick
         except Exception as exc:
             log.error('echonest: search failed: {0}: {1}'
                       .format(util.syspath(item.path), str(exc)))
@@ -66,12 +128,12 @@ class EchonestMetadataPlugin(plugins.BeetsPlugin):
     def profile(self, item):
         try:
             if not item.mb_trackid:
-                raise Exception('musicbrainz ID not available')
+                raise Exception(u'musicbrainz ID not available')
             mbid = 'musicbrainz:track:{0}'.format(item.mb_trackid)
-            track = pyechonest.track.track_from_id(mbid)
-            songs = pyechonest.song.profile(track.song_id,
-                                            buckets=['id:musicbrainz',
-                                                     'audio_summary'])
+            track = self._echofun(pyechonest.track.track_from_id, identifier=mbid)
+            songs = self._echofun(pyechonest.song.profile, ids=track.song_id,
+                    buckets=['id:musicbrainz', 'audio_summary'])
+            # FIXME: can we trust this or should we double check duration?
             return songs[0]
         except Exception as exc:
             log.error('echonest: profile failed: {0}: {1}'
@@ -82,8 +144,10 @@ class EchonestMetadataPlugin(plugins.BeetsPlugin):
             try:
                 song = method(item)
                 if song:
-                    log.debug('echonest: got song through {0}: {1}'
-                              .format(method.im_func.func_name, song))
+                    log.debug('echonest: got song through {0}: {1} - {2} [{3}]'
+                              .format(method.im_func.func_name,
+                              song.artist_name, song.title,
+                              song.audio_summary['duration']))
                     return song
             except Exception as exc:
                 log.error('echonest: tagging: {0}: {1}'
@@ -93,6 +157,7 @@ class EchonestMetadataPlugin(plugins.BeetsPlugin):
         if item.path in self._songs:
             item.echonest_id = self._songs[item.path].id
             for k, v in self._songs[item.path].audio_summary.iteritems():
+                log.info(u'echonest: metadata: {0} - {1}'.format(k, v))
                 setattr(item, k, v)
             if config['import']['write'].get(bool):
                 log.info(u'echonest: writing metadata: {0}'
@@ -112,3 +177,19 @@ class EchonestMetadataPlugin(plugins.BeetsPlugin):
     def apply_metadata_task(self, task, session):
         for item in task.imported_items():
             self.apply_metadata(item)
+
+    def commands(self):
+        cmd = ui.Subcommand('echonest',
+            help='Fetch metadata from the EchoNest')
+
+        def func(lib, opts, args):
+          for item in lib.items(ui.decargs(args)):
+              log.info(u'echonest: {0} - {1} [{2}]'.format(item.artist,
+                    item.title, item.length))
+              self._songs[item.path] = self.fetch_song(item)
+              self.apply_metadata(item)
+
+        cmd.func = func
+        return [cmd]
+
+# eof
